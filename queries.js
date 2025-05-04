cc12a65f5ecf9f4267ad7bdd4bf0e389128f867e
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-05-04 19:26:24 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -6,23 +6,23 @@ const Pool = require('pg').Pool // a connection object to Postgres
 
 const pool = new Pool({ 
 
-user: process.env.DB_USERNAME, 
-host: process.env.DB_HOST, 
-database: process.env.DB_NAME, 
-password: process.env.DB_PASSWORD,  
-port: process.env.DB_HOST 
+    user: process.env.DB_USERNAME, 
+    host: process.env.DB_HOST, 
+    database: process.env.DB_NAME, 
+    password: process.env.DB_PASSWORD,  
+    port: process.env.DB_PORT 
 })  
 
 const getFavLinks (req, res) { 
     // use pg to get our data from the database and return it to the user   
     pool.query(SELECT * FROM favlinks', (error, result)=>{   
-        if (error) { 
-             // do something with the error!  
-             //res.status(400).json("Something went wrong") 
-             throw error 
-         } else { 
-             console.log(result)
-             res.status(200).json(result.rows) 
+        if(error){ 
+            // do something with the error!  
+            //res.status(400).json("Something went wrong") 
+            throw error 
+        } else { 
+            console.log(result)
+            res.status(200).json(result.rows) 
          }
     
     })
